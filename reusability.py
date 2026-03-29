541e11da3cc82d1e0973f411bd03caa084efd9e6
JogaSanthosh303 <joga.santosh303@gmail.com>
2026-03-29 15:39:27 +0530

    Added functions

---
+++ reusability.py
@@ -0,0 +1,3 @@
+import function   # here import function file. so functions in that file is available here
+
+function.sum(3,5)    # result -> This is the sum :  8 . here code reuse by using import keywprd
